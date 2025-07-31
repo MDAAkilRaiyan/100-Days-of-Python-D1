8a6c30dc351928c2ca6cd695060a4a30579076ad
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-07-31 22:14:24 +0600

    Debugging Test Completed

---
+++ 009_Debugging_Practice.py
@@ -0,0 +1,1 @@
+print("Notes from Day 1\nThe print statement is used to output strings\nStrings are strings of characters\nString Concatenation is done with the + sign\nNew lines can be created with a \ and the letter n\n")
