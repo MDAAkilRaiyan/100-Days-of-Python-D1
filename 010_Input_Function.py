4883466a65d069dd1cf107838f6c2072259bb918
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-01 00:17:15 +0600

    Added concatenation with Input function

--- 010_Input_Function.py
+++ 010_Input_Function.py
@@ -2,4 +2,4 @@
 
 # To take any Input from an User, the input function is used
 Sup = input("What is the best superhero of all time? ")
-print(Sup)
+print("The Best Super Hero of All Time Is " + Sup)
