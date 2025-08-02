9de387eb1d99db16b55bbaa762e871d3eb23cb44
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-02 12:29:17 +0600

    Used the String conversion funtion for this

--- 011_Python_Variables.py
+++ 011_Python_Variables.py
@@ -4,5 +4,9 @@ name = "Md. Aakil Raiyan"
 
 name_length = len(name)
 
+# without Converting to String
 print("Hello " + name + "\nYour Name Length is -")
 print(name_length)
+
+# With converting to String
+print(name + " is a name with a total of " + str(name_length) + " letters")
