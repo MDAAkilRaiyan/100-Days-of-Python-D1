060195f5f682f386c7f8f6a384fe52598bb91935
MDAakilRaiyan <aakil15-11060@diu.edu.bd>
2025-08-03 22:49:34 +0600

    Day one Final Project of Band Name Generator Done

---
+++ 015_Band_Name_Generator.py
@@ -0,0 +1,8 @@
+# Band Name Generator Final Project for Day 1
+print("Welcome to the Band Name Generator")
+
+city_name = input("What's The Name of The City Your Grew Up in?\n")
+
+pet_name = input("What's Your Pet's Name?\n")
+
+print("Your Band Name Could Be " + city_name + " " + pet_name)
